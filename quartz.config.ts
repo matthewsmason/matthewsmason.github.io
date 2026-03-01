38b70a7e572dc3d2f1663cc2af4f91d1f7ef8bc0
Matthew <mmas190@wgu.edu>
2026-03-01 15:42:51 -0600

    Quartz sync: Mar 1, 2026, 3:42 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "matthewmason.blog",
     ignorePatterns: ["private", "templates", ".obsidian", "Assets"],
     defaultDateType: "modified",
     theme: {
