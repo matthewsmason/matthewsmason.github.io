f4741577cde92cee16212516ece5838c9b3fce84
Matthew <mmas190@wgu.edu>
2026-03-01 15:33:56 -0600

    Quartz sync: Mar 1, 2026, 3:33 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+		filterFn: (node) => node.name !== "Assets",
+	}),
   ],
   right: [
     Component.Graph(),
@@ -62,7 +64,9 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+		filterFn: (node) => node.name !== "Assets",
+	}),
   ],
   right: [],
 }
